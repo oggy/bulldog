219d35af302a3d9ad8d5ab18a85222dc07bad2fc
George Ogata <george.ogata@gmail.com>
2009-08-26 18:08:13 -0400

    Add spec for StyleSet.

---
+++ spec/style_set_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe StyleSet do
+  describe "#[]" do
+    before do
+      @one = Style.new(:one, {})
+      @two = Style.new(:two, {})
+    end
+
+    it "should allow lookup by style name" do
+      style_set = StyleSet[@one, @two]
+      style_set[:one].should equal(@one)
+      style_set[:two].should equal(@two)
+    end
+
+    it "should still allow lookup by index" do
+      style_set = StyleSet[@one, @two]
+      style_set[0].should equal(@one)
+      style_set[1].should equal(@two)
+    end
+  end
+end
