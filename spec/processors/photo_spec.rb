1f51857573b6a4211c6abb88140268039590f7f7
George Ogata <george.ogata@gmail.com>
2009-09-02 16:37:30 -0400

    Fix example group name.

--- spec/processors/photo_spec.rb
+++ spec/processors/photo_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Processor::Base do
+describe Processor::Photo do
   before do
     @original_convert_command = Processor::Photo.convert_command
     Processor::Photo.convert_command = 'CONVERT'
