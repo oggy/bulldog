a6cf5e2eb17be07c647f03ac671c685040de7ae8
George Ogata <george.ogata@gmail.com>
2009-11-12 15:00:43 -0500

    Add dimensions to PDF attachments.

--- lib/bulldog/attachment/pdf.rb
+++ lib/bulldog/attachment/pdf.rb
@@ -1,6 +1,30 @@
 module Bulldog
   module Attachment
     class Pdf < Base
+      #
+      # Return the width and height of the named style, as a 2-element
+      # array.
+      #
+      # For :original, this is based on the output of ImageMagick's
+      # <tt>identify</tt> command.  Other styles are calculated from
+      # the original style's dimensions, plus the style's :size and
+      # :filled attributes.
+      #
+      # +style_name+ defaults to the attribute's #default_style.
+      #
+      def dimensions(style_name)
+        if style_name.equal?(:original)
+          examine
+          @original_dimensions
+        else
+          style = reflection.styles[style_name]
+          target_dimensions = style[:size].split(/x/).map(&:to_i)
+          resized_dimensions(dimensions(:original), target_dimensions, style[:filled])
+        end
+      end
+
+      include HasDimensions
+
       protected  # ---------------------------------------------------
 
       #
@@ -8,6 +32,29 @@ module Bulldog
       #
       def default_processor_type
         :image_magick
+      end
+
+      #
+      # Read the original image metadata with ImageMagick's identify
+      # command.
+      #
+      def run_examination
+        if stream.missing?
+          @original_dimensions = [1, 1]
+          false
+        else
+          output = `identify -format "%w %h %[exif:Orientation]" #{stream.path}[0] 2> /dev/null`
+          if $?.success? && output.present?
+            width, height, orientation = *output.scan(/(\d+) (\d+) (\d?)/).first.map(&:to_i)
+            rotated = (orientation & 0x4).nonzero?
+            @original_dimensions ||= rotated ? [height, width] : [width, height]
+            true
+          else
+            Bulldog.logger.warn "command failed (#{$?.exitstatus})"
+            @original_dimensions = [1, 1]
+            false
+          end
+        end
       end
     end
   end
--- spec/unit/attachment/pdf_spec.rb
+++ spec/unit/attachment/pdf_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe Attachment::Pdf do
   set_up_model_class :Thing do |t|
-    t.string :attachment_file_name
+    t.integer :attachment_width
+    t.integer :attachment_height
+    t.float :attachment_aspect_ratio
+    t.string :attachment_dimensions
   end
 
   before do
-    Thing.has_attachment :attachment
+    Thing.has_attachment :attachment do
+      style :double, :size => '1224x1584'
+      style :filled, :size => '500x500', :filled => true
+      style :unfilled, :size => '1000x1000'
+      default_style :double
+    end
     @thing = Thing.new(:attachment => test_file)
   end
 
@@ -32,6 +40,98 @@ describe Attachment::Pdf do
 
       @thing.attachment.process(:event)
       context.should be_a(Processor::ImageMagick)
+    end
+  end
+
+  describe "#dimensions" do
+    it "should return 1x1 if the style is missing" do
+      Thing.attachment_reflections[:attachment].configure do
+        when_file_missing do
+          use_attachment(:pdf)
+        end
+      end
+      @thing.save.should be_true
+      File.unlink(@thing.attachment.path(:original))
+      @thing = Thing.find(@thing.id)
+      @thing.attachment.dimensions(:original).should == [1, 1]
+    end
+
+    it "should return the width and height of the default style if no style name is given" do
+      @thing.attachment.dimensions.should == [1224, 1584]
+    end
+
+    it "should return the width and height of the given style" do
+      @thing.attachment.dimensions(:original).should == [612, 792]
+      @thing.attachment.dimensions(:double).should == [1224, 1584]
+    end
+
+    it "should return the calculated width according to style filledness" do
+      @thing.attachment.dimensions(:filled).should == [500, 500]
+      @thing.attachment.dimensions(:unfilled).should == [773, 1000]
+    end
+
+    it "should only invoke identify once"
+    it "should log the result"
+  end
+
+  describe "#width" do
+    it "should return the width of the default style if no style name is given" do
+      @thing.attachment.width.should == 1224
+    end
+
+    it "should return the width of the given style" do
+      @thing.attachment.width(:original).should == 612
+      @thing.attachment.width(:double).should == 1224
+    end
+  end
+
+  describe "#height" do
+    it "should return the height of the default style if no style name is given" do
+      @thing.attachment.height.should == 1584
+    end
+
+    it "should return the height of the given style" do
+      @thing.attachment.height(:original).should == 792
+      @thing.attachment.height(:double).should == 1584
+    end
+  end
+
+  describe "#aspect_ratio" do
+    it "should return the aspect ratio of the default style if no style name is given" do
+      @thing.attachment.aspect_ratio.should be_close(612.0/792, 1e-5)
+    end
+
+    it "should return the aspect ratio of the given style" do
+      @thing.attachment.aspect_ratio(:original).should be_close(612.0/792, 1e-5)
+      @thing.attachment.aspect_ratio(:filled).should be_close(1, 1e-5)
+    end
+  end
+
+  describe "storable attributes" do
+    it "should set the stored attributes on assignment" do
+      @thing.attachment_width.should == 612
+      @thing.attachment_height.should == 792
+      @thing.attachment_aspect_ratio.should be_close(612.0/792, 1e-5)
+      @thing.attachment_dimensions.should == '612x792'
+    end
+
+    describe "after roundtripping through the database" do
+      before do
+        @thing.save
+        @thing = Thing.find(@thing.id)
+      end
+
+      it "should restore the stored attributes" do
+        @thing.attachment_width.should == 612
+        @thing.attachment_height.should == 792
+        @thing.attachment_aspect_ratio.should be_close(612.0/792, 1e-5)
+        @thing.attachment_dimensions.should == '612x792'
+      end
+
+      it "should recalculate the dimensions correctly" do
+        @thing.attachment.dimensions(:filled).should == [500, 500]
+        @thing.attachment.dimensions(:unfilled).should == [773, 1000]
+      end
     end
   end
 end
